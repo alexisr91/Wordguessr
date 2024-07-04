a637ecf2da79ab7f1ce06f0f4bceb34151eb52eb
Alexis <fkahope@Mac.local>
2024-07-04 20:18:16 +0200

    tweaking again

--- src/main/java/com/alexis/WordGuessr/controller/GameController.java
+++ src/main/java/com/alexis/WordGuessr/controller/GameController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import com.alexis.WordGuessr.service.GameService;
 
 
@@ -16,8 +18,9 @@ public class GameController {
     
     
     @GetMapping("/game-home")
-    public String showGameHomePage(Model model){
-
+    public String showGameHomePage(@RequestParam(value ="guessedChar", required = false) String guessedChar ,Model model){
+        // extract query parameters, form parameters, and even files from the request.
+        // required is by default true, if it's false so NULL that means guessedChar will be present in the request to avoid an exception
         String randomWord = gameService.toString();
         model.addAttribute("wordToDisplay", randomWord);
         // The attribute's name will be picked in the template part with Thymeleaf, the second parameter is the array that includes the random word 
