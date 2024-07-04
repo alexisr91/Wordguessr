bf3794de71b6ea3d7bad2321027b9bdfdc859474
Alexis <fkahope@Mac.local>
2024-07-04 12:06:49 +0200

    Hiding the choosen words

--- src/main/java/com/alexis/WordGuessr/service/GameService.java
+++ src/main/java/com/alexis/WordGuessr/service/GameService.java
@@ -8,13 +8,37 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class GameService {
+
+    private String randomlyChoosenWord= null;
+    Random random = new Random();
     
     private String[] randomWords = {"father", "mother", "sister", "goodbye", "software", "framework"};
 
-    Random random = new Random();
+    private char[] allCharactersOfTheWord;
+
+
+    public GameService(){
+
+        randomlyChoosenWord = randomWords[random.nextInt(randomWords.length)];
+        System.out.println("randomlyChoosenWord is : " + randomlyChoosenWord);
+        allCharactersOfTheWord = new char[randomlyChoosenWord.length()];
+    }
 
     @Override
     public String toString(){
-        return randomWords[random.nextInt(randomWords.length)];
+
+        String ret = "";
+
+        for(char c:allCharactersOfTheWord){
+            if(c == '\u0000'){
+                // In unicode \u0000 means null
+                ret = ret + " _";
+            }
+
+            ret = ret + " ";
+            // we return an underscore but also a blank to make space in-between the underscore 
+        }
+
+        return ret;
     }
 }
